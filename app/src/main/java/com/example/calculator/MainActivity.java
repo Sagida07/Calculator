8c19fc3476048fdaf84ad73a7ff4f0ca137c0020
Sagida <138651589+Sagida07@users.noreply.github.com>
2023-09-01 13:02:16 +0600

    MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -80,4 +80,28 @@ public class MainActivity extends AppCompatActivity {
         }
         isOperationClicked = true;
     }
+     @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
